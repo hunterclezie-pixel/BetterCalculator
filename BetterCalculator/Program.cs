92da15d234799c107442a22dcc870c977b53b7f3
Hunter Clezie <clezhunt@gmail.com>
2026-01-29 09:06:45 -0700

    Fixed calculation function

--- BetterCalculator/Program.cs
+++ BetterCalculator/Program.cs
@@ -19,6 +19,7 @@ namespace BetterCalculator
             bool isValid = false;
             string message = "";
 
+            //First Number
             do
             {
                 Console.WriteLine("Please enter your first number");
@@ -34,10 +35,10 @@ namespace BetterCalculator
                     message = $"Good job! {firstNumber} is a number!";
 
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     if (userinput == "Q" || userinput == "q")
-                    {
+                    {                                                                                       
                         Console.WriteLine("Have a nice day!");
                         isValid = true;
                     }
@@ -53,6 +54,7 @@ namespace BetterCalculator
 
             message = $"Good job! {firstNumber} is a number!";
 
+            //Second Number
             do
             {
                 Console.WriteLine("Please enter your second number");
@@ -65,10 +67,10 @@ namespace BetterCalculator
                     // risky code
                     secondNumber = int.Parse(userinput);
                     isValid = true;
-                    message = $"Good job! {firstNumber} is a number!";
+                    message = $"Good job! {secondNumber} is a number!";
 
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     if (userinput == "Q" || userinput == "q")
                     {
@@ -79,6 +81,7 @@ namespace BetterCalculator
                     else
                     {
                         Console.WriteLine($"{userinput} is not a number");
+                        isValid = false;
                     }
                 }
 
@@ -87,12 +90,27 @@ namespace BetterCalculator
 
             message = $"Good job! {secondNumber} is a number!";
 
+            //Third Number
             do
             {
                 Console.WriteLine("Please enter a number to select how you'd like to solve! (1-4)");
                 Console.WriteLine("Enter \"Q\" to exit");
                 userinput = Console.ReadLine();
-                Console.WriteLine($"You enter {userinput}");
+                switch (userinput)
+                {
+                    case "1":
+                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                        break;
+                    case "4":
+                        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                        break;
+                }
 
                 try
                 {
@@ -102,7 +120,7 @@ namespace BetterCalculator
                     message = $"Good job! {thirdNumber} is a number!";
 
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     if (userinput == "Q" || userinput == "q")
                     {
@@ -110,33 +128,10 @@ namespace BetterCalculator
                         isValid = true;
                     }
 
-                    else if (userinput = "1")
-                    {
-                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-                        isValid = true;
-                    }
-
-                    else if (userinput = "2")
-                    {
-                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-                        isValid = true;
-                    }
-
-                    else if (userinput = "3")
-                    {
-                        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
-                        isValid = true;
-                    }
-
-                    else if (userinput = "4")
-                    {
-                        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-                        isValid = true;
-                    }
-
                     else
                     {
                         Console.WriteLine($"{userinput} is not a number");
+                        isValid = false;
                     }
                 }
 
@@ -145,6 +140,7 @@ namespace BetterCalculator
 
             message = $"Good job! {thirdNumber} is a number!";
 
+            //Pause
             Console.Read();
 
         }
